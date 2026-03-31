a0517136ef6d160309b1f562e5b1b203799d2214
Lioquacht-Religion <basik0010@gmail.com>
2026-03-31 23:37:25 +0200

    - added start service and continued install script

--- scripts/install_script.fish
+++ scripts/install_script.fish
@@ -1,2 +1,6 @@
+git pull
+cargo run --release
+fish scripts/create_dist_dir.fish
 psql -d schili_sensor_db -a -f "sql/sensors.sql"
+
 
---
+++ scripts/start_service.fish
@@ -0,0 +1,22 @@
+function kill_process
+	pkill -f -u $USER $argv
+	if test $status -eq 0; then
+		echo "Successfully shutdown old running service ${argv}.'"
+	else if test $status -eq 1; then
+		echo 'Could not find or shutdown old running service ${argv}.'
+	else
+		echo 'An error occured while trying to shutdown service ${argv}.'
+	end
+end
+
+kill_process 'sensor_data_server'
+echo 'Enabling posgreql service.'
+sudo systemctl enable postgresql.service
+kill -0 'mosquitto'
+if test $status -eq 0; then
+	echo 'Mosquitto mqtt broker already running.'
+else 
+	echo 'Starting mosquitto mqtt broker.'
+        mosquitto -c mosquitto.conf
+end
+./dist/sensor_data_server &
