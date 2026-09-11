c9b1504a4cc5433c5a4e0956038170daa132307b
Lioquacht-Religion <basik0010@gmail.com>
2026-09-11 21:46:52 +0200

    use measure time send by sensor instead of server

--- src/service.rs
+++ src/service.rs
@@ -630,8 +630,7 @@ pub async fn insert_bundled_measurements(
         .map_err(|_| vec![anyhow!("Could not find sensor by reference='{}'.", sensor_ref)])?;
 
     let mut errors: Vec<anyhow::Error> = Vec::new();
-    for m in api_measurements.measurements.iter_mut(){
-        m.measure.measure_time = Utc::now();
+    for m in api_measurements.measurements.iter(){
         if let Err(e) = insert_measurement(pool, sensor.sensor_id, m.sensor_type, &m.measure).await {
             errors.push(e);
         }
